8eb107974b96b8983efb12487e08ac72465fe624
tylteng <teng.ty4@gmail.com>
2017-12-18 15:10:10 -0500

    added environment key into .bash_profile and called upon it in for git api

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,7 +1,23 @@
 class PokemonController < ApplicationController
 
   def show
-    render json: {"message": "ok"}
+    response = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/")
+    body = JSON.parse(response.body)
+
+    name = body["name"]
+    array_types = body["types"]
+    id = body["id"]
+
+    types = []
+    array_types.each do |type|
+      types << type["type"]["name"]
+    end
+
+    res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV['GIPHY_KEY']}&q=#{name}&rating=g")
+    body = JSON.parse(res.body)
+    url = body["data"].sample["url"]
+
+    render json: { id: id, name: name, types: types, gif: url}
   end
 
 end
